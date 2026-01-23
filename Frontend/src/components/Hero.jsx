fbee2f30ff3c3da36179c0d33d7343df50a735aa
Nagy Kristof <nagykris62@gmail.com>
2026-01-23 09:24:39 +0100

    aproság

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import "./Hero.css"
+import { Link } from "react-router-dom";
 
 export default function Hero() {
   return (
@@ -6,7 +7,7 @@ export default function Hero() {
       <div className="hero-content">
         <h1>HETI AKCIÓK</h1>
         <p>20% - 50%</p>
-        <button className="btn">RENDELJ MOST</button>
+        <Link to="/parfumok"><button className="btn">RENDELJ MOST</button></Link>
       </div>
     </section>
   );
